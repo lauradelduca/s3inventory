0f2200b15b0fea732f08119f94112196fee1f399
lauradelduca <lauraseegerer@gmail.com>
2018-04-19 10:41:56 +0200

    codes to download

--- comtrade_check.R
+++ comtrade_check.R
@@ -702,3 +702,22 @@ for (cc in countries){
 	}
 
 }
+
+
+## codes for release
+
+todownload <- data.frame(file = CD$file, release = '')
+todownload$release <- as.character(todownload$release)
+
+for (f in as.vector(todownload$file)){
+
+	release <- as.vector( strsplit(as.character(CD$release[CD$file == f][1]), ', ') )
+	hs6_release <- as.vector(as.numeric(hs$code_value[ hs$com_name %in% release[[1]] ]))
+	hs6_release <- as.character(formatC(hs6_release, width = 6, format = "d", flag = "0"))
+	hs2_release <- sort( unique( substr(hs6_release, 1, 2) ) )
+	
+	todownload$release[todownload$file == f] <- paste(hs2_release, collapse=", ")
+
+}
+
+write.table(todownload, 'CD_todownload.csv', quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
