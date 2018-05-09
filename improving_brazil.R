43808abca335e4502bca83ef8912858ab20ad84c
Laura Del Duca <laura.delduca@sei.org>
2018-05-09 10:25:05 +0200

    backup

--- improving_brazil.R
+++ improving_brazil.R
@@ -8,3 +8,11 @@ options(scipen=99999999)
 
 # CD_BRAZIL_2017.csv
 
+din <- 'C:/Users/laura.delduca/Desktop/code/0504/brazil'
+setwd(din)
+ff <- list.files(din, pattern = 'csv', full = TRUE)
+
+
+
+write.table(b17sn, 'CD_BRAZIL_2017_test1.csv', quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+
