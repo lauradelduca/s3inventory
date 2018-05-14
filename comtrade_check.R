962ddc447e2aee2b87ef318148ea201ee8875b99
Laura Del Duca <laura.delduca@sei.org>
2018-05-14 17:29:24 +0200

    comtrade_check.R style

--- comtrade_check.R
+++ comtrade_check.R
@@ -1,29 +1,37 @@
-## final comtrade check
+## Checking customs declarations trade data against COMTRADE for 2018 level 1 release
+## Laura Del Duca
 
-library(aws.s3)
-library(stringr)
-library(gsubfn)
-library(dplyr)
+require(stringr)
+require(gsubfn)
+require(dplyr)
+require(readxl)
+require(data.table)
+require(libamtrack)
+require(aws.s3)
 
 options(scipen=9999999999)
 
+setwd('C:/Users/laura.delduca/Desktop/code')
+current_folder <- '0515'
+
+aws_credentials_file <- 'R_aws.s3_credentials.R'
+source(aws_credentials_file)
 
 
 
-
-## get content of AWS -----------------------------
+## get content of AWS
 
 argentina_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/ARGENTINA/')
 argentina_content <- subset(argentina_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
-bolivia_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/BOLIVIA/')
-bolivia_content <- subset(bolivia_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
+# bolivia_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/BOLIVIA/')
+# bolivia_content <- subset(bolivia_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
 brazil_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/BRAZIL/')
 brazil_content <- subset(brazil_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
-colombia_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/COLOMBIA/')
-colombia_content <- subset(colombia_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
+# colombia_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/COLOMBIA/')
+# colombia_content <- subset(colombia_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
 costarica_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/COSTA RICA/')
 costarica_content <- subset(costarica_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
@@ -31,11 +39,11 @@ costarica_content <- subset(costarica_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}
 ecuador_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/ECUADOR/')
 ecuador_content <- subset(ecuador_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
-mexico_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/MEXICO/')
-mexico_content <- subset(mexico_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
+# mexico_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/MEXICO/')
+# mexico_content <- subset(mexico_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
-panama_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/PANAMA/')
-panama_content <- subset(panama_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
+# panama_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/PANAMA/')
+# panama_content <- subset(panama_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
 paraguay_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/PARAGUAY/')
 paraguay_content <- subset(paraguay_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
@@ -46,38 +54,34 @@ peru_content <- subset(peru_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Ke
 uruguay_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/URUGUAY/')
 uruguay_content <- subset(uruguay_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
-venezuela_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/VENEZUELA/')
-venezuela_content <- subset(venezuela_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
+# venezuela_content <- get_bucket_df(bucket = 'trase-storage', prefix = 'data/1-TRADE/CD/EXPORT/VENEZUELA/')
+# venezuela_content <- subset(venezuela_content, grepl(".*/CD_[A-Z]+_[1-9][0-9]{3}.csv$", Key) )
 
 
 CD_content <- do.call(rbind, list(	argentina_content,
-									bolivia_content,
+									# bolivia_content,
 									brazil_content,
-									colombia_content,
+									# colombia_content,
 									costarica_content,
 									ecuador_content,
-									mexico_content,
-									panama_content,
+									# mexico_content,
+									# panama_content,
 									paraguay_content,
 									peru_content,
-									uruguay_content,
-									venezuela_content	))
+									uruguay_content #,
+									# venezuela_content	
+									))
 
-					
 CD <- data.frame(	file = CD_content$Key,
 					type = strapplyc(CD_content$Key, ".*/(CD)_[A-Z]+_[1-9][0-9]{3}.csv$", simplify = TRUE),
 					country = strapplyc(CD_content$Key, ".*/CD_([A-Z]+)_[1-9][0-9]{3}.csv$", simplify = TRUE),
-					year = strapplyc(CD_content$Key, ".*/CD_[A-Z]+_([1-9][0-9]{3}).csv$", simplify = TRUE)  )
-					
+					year = strapplyc(CD_content$Key, ".*/CD_[A-Z]+_([1-9][0-9]{3}).csv$", simplify = TRUE)  )		
 CD <- CD[CD$year != 2018,]
-
 CD <- CD[order(CD$country, CD$year),]	
 
 
 
-
-
-## get relevant hs codes from commodity dictionary ----------------------------
+## get relevant hs codes from commodity dictionary
 
 obj <- get_object(object = 'data/1-TRADE/commodity_equivalents_final.csv', bucket = 'trase-storage')
 
@@ -101,7 +105,7 @@ sugarcane <- as.vector(as.numeric(sort(unique(hs$code_value[hs$com_name == 'SUGA
 
 
 
-## load preprocessed comtrade files -----------------------------
+## load preprocessed COMTRADE files
 
 obj <- get_object(object = 'data/1-TRADE/STATISTICAL_DATA/GLOBAL/COMTRADE/COMTRADE_ZOOM/COMTRADE_2005_zoom.csv', bucket = 'trase-storage')
 comtrade05 <- read.csv(text = rawToChar(obj), quote = '', sep = ';')
@@ -131,9 +135,7 @@ comtrade16 <- read.csv(text = rawToChar(obj), quote = '', sep = ';')
 
 
 
-
-
-## select columns for each country/dataset --------------------------------
+## select columns for each country/dataset
 
 for (f in as.vector(CD$file)){
 	
@@ -145,24 +147,19 @@ for (f in as.vector(CD$file)){
 		CD$units_column[CD$file == f] <- units_column <- 'UNIDAD_ESTADSTICA'
 			
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Quantity.1'
-			#CD$weight_column[CD$file == f] <- weight_column <- 'Cantidad.Estadística'
-			
-			CD$weight_column_2[CD$file == f] <- weight_column_2 <- 'Cantidad.Estadística'
-			CD$units_column[CD$file == f] <- units_column <- 'Unidad.Estadística'
-			
+			#CD$weight_column[CD$file == f] <- weight_column <- 'Cantidad.Estadistica'
+			CD$weight_column_2[CD$file == f] <- weight_column_2 <- 'Cantidad.Estadistica'
+			CD$units_column[CD$file == f] <- units_column <- 'Unidad.Estadistica'
 		}
 		
 		if (grepl('SOURCE/CD_ARGENTINA_2010.csv', f)){
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.CodeProduct.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value.US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'Cantidad.Estadstica'
 			CD$units_column[CD$file == f] <- units_column <- 'Unidad.Estadstica'
-		
 		}
 		
 		release <- c('BEEF', 'CHICKEN', 'CORN', 'COTTON', 'LEATHER', 
@@ -174,23 +171,17 @@ for (f in as.vector(CD$file)){
 	if (CD$country[CD$file == f] == 'BOLIVIA'){
 	
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'
-			
 		} else if (grepl("CD_BOLIVIA_2010", f)){
-			
 			CD$hs_column[CD$file == f] <- hs_column <- 'COD_ARMONIZADOPRODUCTO_INGLES'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_VALOR_FOB_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_QUANTITY_1'
-		
 		} else{
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'COD_ARMONIZADOPRODUCTO_INGLES'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_VALOR_FOB_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_PESO_NETO_KG'
-		
 		}
 			
 		release <- c('CHICKEN', 'COFFEE', 'CORN', 'LEATHER', 
@@ -218,11 +209,9 @@ for (f in as.vector(CD$file)){
 		CD$weight_column[CD$file == f] <- weight_column <- 'PESO_LIQ_MERC_BAL_EXP'
 		
 		if (grepl('2017', f){
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'COD.SUBITEM.NCM'
 			CD$price_column[CD$file == f] <- price_column <- 'VMLE.DOLAR.BAL.EXP'
 			CD$weight_column[CD$file == f] <- weight_column <- 'PESO.LIQ.MERC.BAL.EXP'
-
 		}
 		
 		release <- c('BEEF', 'CHICKEN', 'COCOA', 'COFFEE', 'CORN', 'COTTON', 'LEATHER', 
@@ -234,17 +223,13 @@ for (f in as.vector(CD$file)){
 	if (CD$country[CD$file == f] == 'COLOMBIA'){
 	
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'
-			
 		} else{
-			
 			CD$hs_column[CD$file == f] <- hs_column <- 'HARMONIZED_CODE_PRODUCT_ENGLISH'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_FOB_VALUE_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_NET_WEIGHT_KG'
-		
 		}
 
 		release <- c('BEEF', 'CHICKEN', 'COCOA', 'COFFEE', 'CORN', 'LEATHER', 
@@ -280,17 +265,13 @@ for (f in as.vector(CD$file)){
 	if (CD$country[CD$file == f] == 'MEXICO'){
 		
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Quantity.1'
-			
 		} else{
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'HARMONIZED_CODE_PRODUCT_ENGLISH'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_FOB_VALUE_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_QUANTITY_1'
-		
 		}
 	
 		release <- c('BEEF', 'CHICKEN', 'COCOA', 'COFFEE', 'CORN', 'COTTON', 'LEATHER', 
@@ -302,17 +283,13 @@ for (f in as.vector(CD$file)){
 	if (CD$country[CD$file == f] == 'PANAMA'){
 		
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'
-			
 		} else{
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'HARMONIZED_CODE_PRODUCT_ENGLISH'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_FOB_VALUE_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_NET_WEIGHT_KG'
-		
 		}
 		
 		release <- c('COFFEE', 'LEATHER', 
@@ -352,21 +329,15 @@ for (f in as.vector(CD$file)){
 		CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight.Kg'
 		
 		if (grepl("SICEX25", f)){
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'  
-			
 		}
-		
 		if (grepl("CD_PERU_2016.csv", f) | grepl("CD_PERU_2017.csv", f)){
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'COD..ARMONIZADOPRODUCTO.INGLES'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.VALOR.FOB.US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.PESO.NETO.KG'
-		
-		} 
-		
+		}
 		
 		release <- c('CHICKEN', 'COCOA', 'COFFEE', 'CORN', 'LEATHER', 
 					'TIMBER', 'PALM OIL', 'SHRIMPS', 'SUGAR CANE')
@@ -376,21 +347,15 @@ for (f in as.vector(CD$file)){
 	
 	if (CD$country[CD$file == f] == 'URUGUAY'){
 
-		# 2012 no, some way too small, always except for beef
 		if ((grepl('/URUGUAY/2012/CD_URUGUAY_2012.csv', f)) ){
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'TOTAL_NET_WEIGHT_KG'
 			CD$price_column[CD$file == f] <- price_column <- 'HARMONIZED_CODEPRODUCT_ENGLISH'
 			CD$weight_column[CD$file == f] <- weight_column <- 'MEASURE_UNIT_1_QUANTITY_1'
-			
 		}
-		
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.English'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'
-		
 		} 
 		
 		release <- c('BEEF', 'CHICKEN', 'COCOA', 'CORN', 'LEATHER', 
@@ -402,17 +367,13 @@ for (f in as.vector(CD$file)){
 	if (CD$country[CD$file == f] == 'VENEZUELA'){
 		
 		if (grepl("SICEX25", f)){
-	
 			CD$hs_column[CD$file == f] <- hs_column <- 'Harmonized.Code.Product.Spanish'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL.FOB.Value..US..'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL.Net.Weight..Kg.'
-		
 		} else{
-		
 			CD$hs_column[CD$file == f] <- hs_column <- 'HARMONIZED_CODE_PRODUCT_SPANISH'
 			CD$price_column[CD$file == f] <- price_column <- 'TOTAL_FOB_VALUE_US'
 			CD$weight_column[CD$file == f] <- weight_column <- 'TOTAL_NET_WEIGHT_KG'		
-		
 		}
 		
 		release <- c('COCOA', 'LEATHER', 
@@ -425,19 +386,15 @@ for (f in as.vector(CD$file)){
 	
 }
 
-write.table(CD, 'CD_AWS.csv', quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+write.table(CD, paste0(current_folder, '/', 'CD_AWS.csv'), quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
 
 
 
 
-
-
-## chose which countries to run -----------------------
+## chose which countries to run
 
 countries <- unique(as.vector(CD$country))
-
 parked <- c('VENEZUELA', 'COLOMBIA', 'PANAMA', 'BOLIVIA', 'MEXICO', 'ARGENTINA', 'BRAZIL')
-
 countries <- countries[!countries %in% parked]
 
 countries <- c('ARGENTINA')
@@ -445,9 +402,7 @@ cc <- 'ARGENTINA'
 
 
 
-
-
-## Trase - COMTRADE weights comparison ------------------------
+## Trase - COMTRADE weights comparison
 
 
 for (cc in countries){
