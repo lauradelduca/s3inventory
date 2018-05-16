30a42efed6a3d561be478a7591c0f8ff1f1b799a
Laura Del Duca <laura.delduca@sei.org>
2018-05-16 16:43:07 +0200

    corrected path

--- comtrade_checks/comtrade_check.R
+++ comtrade_checks/comtrade_check.R
@@ -16,28 +16,27 @@ options(scipen=9999999999)
 setwd('C:/Users/laura.delduca/Desktop/code')
 
 current_folder <- '0516'
-aws_credentials_file <- 'R_aws.s3_credentials.R'
+script_folder <- 's3inventory/comtrade_checks'
 
-countries <- unique(as.vector(CD$country))		# chose which countries to run
+source('R_aws.s3_credentials.R')											# load AWS S3 credentials
+source(paste0(script_folder, '/', 'get_comtrade_files.R'))					# load preprocessed COMTRADE files 2005 - 2016
+source(paste0(script_folder, '/', 'get_hs_codes.R'))						# load relevant HS codes from commodity dictionary
+
+source(paste0(script_folder, '/', 'get_aws_content.R'))						# load content of AWS into dataframe CD
+source(paste0(script_folder, '/', 'get_columns.R'))							# select columns for each country/dataset
+write.table(CD, paste0(current_folder, '/', 'CD_AWS.csv'), quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+
+countries <- unique(as.vector(CD$country))									# chose which countries to run
 # parked <- c('VENEZUELA', 'COLOMBIA', 'PANAMA', 'BOLIVIA', 'MEXICO', 'ARGENTINA', 'BRAZIL')
 # countries <- countries[!countries %in% parked]
 countries <- c('ARGENTINA')
 cc <- 'ARGENTINA'
 
+source(paste0(script_folder, '/', 'write_weight_check_files.R'))			# write weight checks files
+source(paste0(script_folder, '/', 'write_unit_check_files.R'))				# write unit checks files
 
-source(aws_credentials_file)				# load AWS S3 credentials
-source(get_comtrade_files.R)				# load preprocessed COMTRADE files 2005 - 2016
-source(get_hs_codes.R)						# load relevant HS codes from commodity dictionary
+source(paste0(script_folder, '/', 'comtrade_check_helpers.R')) 				# helpers (print header,first lines of all files for 'countries')
+source(paste0(script_folder, '/', 'comtrade_check_helpers_argentina.R'))	# helpers for Argentina
 
-source(get_aws_content.R)					# load content of AWS into dataframe CD
-source(get_columns.R)						# select columns for each country/dataset
-write.table(CD, paste0(current_folder, '/', 'CD_AWS.csv'), quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
-
-source(write_weight_check_files.R)			# write weight checks files
-source(write_unit_check_files.R)			# write unit checks files
-
-source(comtrade_check_helpers.R) 			# helpers (print header,first lines of all files for 'countries')
-source(comtrade_check_helpers_argentina.R)	# helpers for Argentina
-
-source(get_release_codes.R)					# load codes needed for release
+source(paste0(script_folder, '/', 'get_release_codes.R'))					# load codes needed for release
 write.table(todownload, paste0(current_folder, '/', 'CD_todownload.csv'), quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
