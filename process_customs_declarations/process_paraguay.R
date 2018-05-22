4d4f87e001257190d104a09db41d842bf069841e
Laura Del Duca <laura.delduca@sei.org>
2018-05-22 15:41:11 +0200

    start paraguay

---
+++ process_customs_declarations/process_paraguay.R
@@ -0,0 +1,282 @@
+## Preprocessing of Paraguay customs declarations trade data 2007 - 2018 from Ministry of Trade
+## processing includes 2018 but this should not be uploaded as it is an approximation
+## Laura Del Duca
+
+
+## MinTrade data comes in one xlsx file with one sheet per year, varying column names/content
+## open each one, check that file has no obvious errors: has data for correct year, country, rows as expected etc
+
+## enable editing, and on each sheet, replace all ';' with '.'
+## save each sheet as csv, here I did CD_PARAGUAY_YEAR.csv
+
+## xlsx was uploaded to S3 PARAGUAY/MINTRADE/ORIGINALS
+## each csv file in an 'ORIGINALS' folder in the appropriate PARAGUAY/MINTRADE/YEAR
+
+
+
+rm(list=ls(all=TRUE))
+
+require(stringr)
+require(gsubfn)
+require(dplyr)
+require(readxl)
+require(data.table)
+require(libamtrack)
+require(aws.s3)
+
+options(scipen=99999999)
+
+setwd('C:/Users/laura.delduca/Desktop/code')
+current_folder <- '0522'
+script_folder <- 's3inventory/comtrade_checks'
+
+source('R_aws.s3_credentials.R')					# load AWS S3 credentials
+
+
+## 2013 - 2015 SICEX2.5
+for (yy in 2007:2018){
+	
+	# load csv originals keys for all years, store in vector 'peru_originals_YEAR_keys'
+	orig <- get_bucket_df(bucket = 'trase-storage', prefix = paste0('data/1-TRADE/CD/EXPORT/PARAGUAY/MINTRADE/', yy))	
+	keys <- subset(orig, grepl("ORIGINALS/.*.csv$", Key) )
+	keys <- as.vector(keys$Key)
+	assign(paste0('peru_originals_', yy, '_keys'), keys)
+	
+	# create an empty list to store the data of each file
+	J <- list()
+	i = 1
+	
+	for (f in keys){
+		
+		obj <- get_object(object = f, bucket = 'trase-storage')
+		data <- read.csv(text = rawToChar(obj), sep = ';', quote = '', row.names = NULL)
+	
+		# make sure the files look correct, and numbers of columns match, to use same names
+		print(f)
+		print(data[1:3,])
+		print(ncol(data))
+		
+		# remove all empty rows: get index of all rows that have NAs across all columns and remove
+		k <- which( apply(data, 1, function(x) all(is.na(x))) )
+		if(length(k)>0) data<- data[-k,]
+		
+		# use column names of the first files, remove special characters if needed, and assign to all
+		# setting encoding of whole file to utf8: 
+		# fread with encoding = 'UTF-8' option is not sufficient so correcting colnames manually
+		if (i==1)  nn <- names(data)
+		if (i>1)   names(data) <- nn
+		
+		# add the data to the list
+		J[[i]] <- data
+		i <- i + 1
+	}
+
+	# append all data stored in list of data frames in J
+	D <- do.call(rbind, J)
+	
+	
+	# in all columns check again that ; is replaced with .
+	D <- data.frame(lapply(D, function(x) {gsub(";", ".", x)}))
+	
+	# remove commas from numeric columns
+	D$TOTAL.FOB.Value..US.. <- as.numeric(gsub(",", "", D$TOTAL.FOB.Value..US..))
+	D$TOTAL.Net.Weight..Kg. <- as.numeric(gsub(",", "", D$TOTAL.Net.Weight..Kg.))	
+	
+	# make sure HS column is even number of digits, here 6
+	D$Harmonized.Code.Product.English <- as.numeric(as.character(D$Harmonized.Code.Product.English))
+	D$Harmonized.Code.Product.English <- AT.add.leading.zeros(D$Harmonized.Code.Product.English, digits = 6)
+	
+	
+	# just for testing... save a copy locally
+	write.table(	D, 
+					paste0(current_folder, '/', 'CD_PERU_', yy, '_TEST.csv'), 
+					quote = FALSE, 
+					row.names = FALSE, 
+					dec = '.', 
+					sep = ';')
+
+	# write table to S3:
+	# write to an in-memory raw connection
+	zz <- rawConnection(raw(0), "r+")
+	write.table(D, zz, quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+	# upload the object to S3
+	put_object(	file = rawConnectionValue(zz), 
+				bucket = 'trase-storage', 
+				object = paste0('data/1-TRADE/CD/EXPORT/PERU/', yy, '/SICEX25/TEST/CD_PERU_', yy, '.csv') )
+	# close the connection
+	close(zz)
+	
+}
+
+
+## 2016 - 2017 SICEX2.0
+for (yy in 2016:2017){
+	
+	# load csv originals keys for all years, store in vector 'peru_originals_YEAR_keys'
+	orig <- get_bucket_df(bucket = 'trase-storage', prefix = paste0('data/1-TRADE/CD/EXPORT/PERU/', yy))	
+	keys <- subset(orig, grepl("ORIGINALS/.*.csv$", Key) )
+	keys <- as.vector(keys$Key)
+	assign(paste0('peru_originals_', yy, '_keys'), keys)
+	
+	# create an empty list to store the data of each file
+	J <- list()
+	i = 1
+	
+	for (f in keys){
+		
+		obj <- get_object(object = f, bucket = 'trase-storage')
+		data <- read.csv(text = rawToChar(obj), sep = ';', quote = '', row.names = NULL, stringsAsFactors = FALSE)
+	
+		# make sure the files look correct, and numbers of columns match, to use same names
+		print(f)
+		print(data[1:3,])
+		print(ncol(data))
+		
+		# remove all empty rows: get index of all rows that have NAs across all columns and remove
+		k <- which( apply(data, 1, function(x) all(is.na(x))) )
+		if(length(k)>0) data<- data[-k,]
+		
+		# use column names of the first files, remove special characters if needed, and assign to all
+		# setting encoding of whole file to utf8: 
+		# fread with encoding = 'UTF-8' option is not sufficient so correcting colnames manually
+		if (i==1)  nn <- names(data)
+		if (i>1)   names(data) <- nn
+		
+		# add the data to the list
+		J[[i]] <- data
+		i <- i + 1
+	}
+
+	# append all data stored in list of data frames in J
+	D <- do.call(rbind, J)
+	
+	
+	# in all columns check again that ; is replaced with .
+	D <- data.frame(lapply(D, function(x) {gsub(";", ".", x)}))
+	
+	# remove commas from numeric columns
+	D$TOTAL.Cantidad.1 <- as.numeric(gsub(",", "", D$TOTAL.Cantidad.1))
+	D$TOTAL.Valor.FOB.US <- as.numeric(gsub(",", "", D$TOTAL.Valor.FOB.US))	
+	D$Valor.FOB.Unitario.US.Cantidad.1 <- as.numeric(gsub(",", "", D$Valor.FOB.Unitario.US.Cantidad.1))
+	D$TOTAL.Peso.Neto.Kg <- as.numeric(gsub(",", "", D$TOTAL.Peso.Neto.Kg))
+	D$TOTAL.Peso.Bruto.Kg <- as.numeric(gsub(",", "", D$TOTAL.Peso.Bruto.Kg))
+	D$TOTAL.Cantidad.2 <- as.numeric(gsub(",", "", D$TOTAL.Cantidad.2))
+	
+	# make sure HS column is even number of digits, here 6
+	D$Cod..ArmonizadoProducto.Ingles <- as.numeric(as.character(D$Cod..ArmonizadoProducto.Ingles))
+	D$Cod..ArmonizadoProducto.Ingles <- AT.add.leading.zeros(D$Cod..ArmonizadoProducto.Ingles, digits = 6)
+	# this should be 10 digits:
+	D$Posicion <- as.numeric(as.character(D$Posicion))
+	D$Posicion <- AT.add.leading.zeros(D$Posicion, digits = 10)
+	
+	
+	# just for testing... save a copy locally
+	write.table(	D, 
+					paste0(current_folder, '/', 'CD_PERU_', yy, '_TEST.csv'), 
+					quote = FALSE, 
+					row.names = FALSE, 
+					dec = '.', 
+					sep = ';')
+
+	# write table to S3:
+	# write to an in-memory raw connection
+	zz <- rawConnection(raw(0), "r+")
+	write.table(D, zz, quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+	# upload the object to S3
+	put_object(	file = rawConnectionValue(zz), 
+				bucket = 'trase-storage', 
+				object = paste0('data/1-TRADE/CD/EXPORT/PERU/', yy, '/SICEX20/TEST/CD_PERU_', yy, '.csv') )
+	# close the connection
+	close(zz)
+	
+}
+
+
+## 2012 SOURCE
+yy <- 2012
+
+# load csv originals keys for all years, store in vector 'peru_originals_YEAR_keys'
+orig <- get_bucket_df(bucket = 'trase-storage', prefix = paste0('data/1-TRADE/CD/EXPORT/PERU/', yy))	
+keys <- subset(orig, grepl("ORIGINALS/.*.csv$", Key) )
+keys <- as.vector(keys$Key)
+assign(paste0('peru_originals_', yy, '_keys'), keys)
+
+# create an empty list to store the data of each file
+J <- list()
+i = 1
+
+for (f in keys){
+	
+	obj <- get_object(object = f, bucket = 'trase-storage')
+	data <- read.csv(text = rawToChar(obj), sep = ';', quote = '', row.names = NULL, stringsAsFactors = FALSE)
+
+	# make sure the files look correct, and numbers of columns match, to use same names
+	print(f)
+	print(data[1:3,])
+	print(ncol(data))
+	
+	# remove all empty rows: get index of all rows that have NAs across all columns and remove
+	k <- which( apply(data, 1, function(x) all(is.na(x))) )
+	if(length(k)>0) data<- data[-k,]
+	
+	# use column names of the first files, remove special characters if needed, and assign to all
+	# setting encoding of whole file to utf8: 
+	# fread with encoding = 'UTF-8' option is not sufficient so correcting colnames manually
+	if (i==1)  nn <- names(data)
+	if (i>1)   names(data) <- nn
+	
+	# add the data to the list
+	J[[i]] <- data
+	i <- i + 1
+}
+
+# append all data stored in list of data frames in J
+D <- do.call(rbind, J)
+
+
+# in all columns check again that ; is replaced with .
+D <- data.frame(lapply(D, function(x) {gsub(";", ".", x)}))
+
+# remove commas from numeric columns
+D$TOTAL.Quantity.1 <- as.numeric(gsub(",", "", D$TOTAL.Quantity.1))
+D$TOTAL.FOB.Value.US <- as.numeric(gsub(",", "", D$TOTAL.FOB.Value.US))	
+D$FOB.per.Unit.Quantity1 <- as.numeric(gsub(",", "", D$FOB.per.Unit.Quantity1))
+D$TOTAL.Net.Weight.Kg <- as.numeric(gsub(",", "", D$TOTAL.Net.Weight.Kg))
+D$TOTAL.Gross.Weight.Kg <- as.numeric(gsub(",", "", D$TOTAL.Gross.Weight.Kg))
+D$TOTAL.Quantity.2 <- as.numeric(gsub(",", "", D$TOTAL.Quantity.2))
+
+# make sure HS column is even number of digits, here 6
+D$Harmonized.CodeProduct.English <- as.numeric(as.character(D$Harmonized.CodeProduct.English))
+D$Harmonized.CodeProduct.English <- AT.add.leading.zeros(D$Harmonized.CodeProduct.English, digits = 6)
+# this should be 10 digits:
+D$Product.Schedule.B.Code <- as.numeric(as.character(D$Product.Schedule.B.Code))
+D$Product.Schedule.B.Code <- AT.add.leading.zeros(D$Product.Schedule.B.Code, digits = 10)
+
+
+# just for testing... save a copy locally
+write.table(	D, 
+				paste0(current_folder, '/', 'CD_PERU_', yy, '_TEST.csv'), 
+				quote = FALSE, 
+				row.names = FALSE, 
+				dec = '.', 
+				sep = ';')
+
+# write table to S3:
+# write to an in-memory raw connection
+zz <- rawConnection(raw(0), "r+")
+write.table(D, zz, quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
+# upload the object to S3
+put_object(	file = rawConnectionValue(zz), 
+			bucket = 'trase-storage', 
+			object = paste0('data/1-TRADE/CD/EXPORT/PERU/', yy, '/SOURCE/TEST/CD_PERU_', yy, '.csv') )
+# close the connection
+close(zz)
+
+
+
+# clean up
+gc()
+
+
+## test new files with comtrade_check.R weight_table
+## correct folder structure on aws
