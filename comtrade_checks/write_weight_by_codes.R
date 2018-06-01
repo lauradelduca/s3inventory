aed1499e95c1d0091686c835f5a03763a1262179
Laura Del Duca <laura.delduca@sei.org>
2018-06-01 10:15:57 +0200

    weights by code

--- comtrade_checks/write_weight_by_codes.R
+++ comtrade_checks/write_weight_by_codes.R
@@ -9,114 +9,44 @@
 
 for (cc in countries){
 	
-	weights_table <- data.frame(commodity = as.vector( strsplit(as.character(CD$release[CD$country == cc][1]), ', ') ))
-	names(weights_table) = c('commodity')
-	
 	for (f in CD$file[CD$country == cc]){
 	
 		obj <- get_object(object = f, bucket = 'trase-storage')
 		data <- read.csv(text = rawToChar(obj), sep = ';', quote = '', row.names = NULL)
 			
-		# if (grepl("ORIGINALS/CD_PARAGUAY_", f)){ data$hs6 <- as.integer(substr(gsub('\\.', '', data$NCM, perl=TRUE), 0, 6)) }
-		
 		if (grepl('BRAZIL', f)){ 
 			data[, CD$hs_column[CD$file == f] ] <- formatC(data[, CD$hs_column[CD$file == f] ], width = 8, format = "d", flag = "0") 
 			data[, CD$hs_column[CD$file == f] ] <- as.integer(substr(data[, CD$hs_column[CD$file == f] ], 0, 6))
 		}
 		
-		# if ((cc == 'PARAGUAY') | (cc == 'BRAZIL') ){
-			# data[, CD$weight_column[CD$file == f] ] <- as.character(data[, CD$weight_column[CD$file == f] ])
-			# for (i in 1:nrow(data)){
-				# data[, CD$weight_column[CD$file == f] ][i] <- gsub(',', '', data[, CD$weight_column[CD$file == f] ][i])
-			# }
-			# data[, CD$weight_column[CD$file == f] ] <- as.numeric(data[, CD$weight_column[CD$file == f] ])
-		# }		
+		# chose only relevant columns
+		data <- data[,c('Year', 
+						'Product.Schedule.B.Code',
+						'Harmonized.Code.Product.English',
+						'Harmonized.Code.Description.English',
+						'Cantidad.Estadistica')]
 		
-		# # comment from here
-		# if (grepl('ARGENTINA', f)){
+		# aggregate by long HS code
 		
-			# for (i in 1:nrow(data)){
-			
-				# # try using another column if weight = 0 in a record
-				# # for soy, using weight_column_2 seems to work
-				# # if (as.numeric(data[, CD$hs_column[CD$file == f] ][i] ) %in% soy){
-					# # data[, CD$weight_column[CD$file == f] ][i] <- data[, CD$weight_column_2[CD$file == f] ][i]
-				# # }
-				
-				# # if (data[, CD$weight_column[CD$file == f] ][i] == 0){
-					# # data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(gsub(',', '', data[, CD$weight_column_2[CD$file == f] ][i]))
-				# # }
+		# filter correct comtrade file by country etc
 		
-				# if (data[, CD$units_column[CD$file == f] ][i] == 'UNIDADES'){
-					# # chicken 010511
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 10511){
-						# # one unidad of live animal is 475kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 475
-					# }
-					# # beef 010221
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 10221){
-						# # one unidad of live animal is 475kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 475
-					# }
-					# # beef 010229
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 10229){
-						# # one unidad of live animal is 475kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 475
-					# }
-				# }
-				
-				# if (data[, CD$units_column[CD$file == f] ][i] == 'DESCONOCIDA'){
-					# # beef 010221
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 10221){
-						# # one unidad of live animal is 475kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 475
-					# }
-					# # beef 010229
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 10229){
-						# # one unidad of live animal is 475kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 475
-					# }
-				# }
-				
-				# if (data[, CD$units_column[CD$file == f] ][i] == 'METROS CUBICOS'){
-					# # timber 440729 from cubic meter to ton conversion factor 0.7 
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) == 440729){
-						# # # 1 cubic meter is 0.7 tons, so 1 cubic meter is 700 kg
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column[CD$file == f] ][i]) * 700
-					# }
-				# }
-				
-				# if (grepl('SICEX25', f)){
-					# if (as.numeric(data[, CD$hs_column[CD$file == f]][i]) %in% c(corn, cotton, woodpulp, soy, sugarcane)){
-						# # if code is of a certain commodity, work with weight_column_2
-						# data[, CD$weight_column[CD$file == f] ][i] <- as.numeric(data[, CD$weight_column_2[CD$file == f] ][i])
-					# }
-				# }
-			# }
-		# }
-		# # until here
+		# keep only relevant columns
 		
+		# rename columns if needed
 		
-		for (i in 1:nrow(weights_table)){
-			
-			hs6_commodity <- as.vector(as.numeric(hs$code_value[ hs$com_name == weights_table$commodity[i] ]))
-
-			data_commodity <- data[ as.numeric( substr(data[, CD$hs_column[CD$file == f] ] , 1, 6)) %in% hs6_commodity, ]
-			
-			# notes on argentina 
-			# get weight whenever there is more than unit kg
-			# countries: argentina 2010 and all others, ecuador
-			# unit_column: UNIDAD_ESTADISTICA argentina
-			# levels: 
-			# conversions:  1 head of live animal is 475 kg
-			#				1 liter of soy oil is 0.92 kilograms of soy
-			# total weight for a year/countries: sum of all weight_column(where unit_column	== x and hs_column %in% soy_codes) * factor 
-			#									+ sum of all weight_column(where unit_column == y and hs_column == b) * factor
-			#									...
-			# include column with, if column exists, levels of unit_column, with ': number of rows in this commodity, '
-			
-			weights_table$new_column[i] <- sum( as.numeric(data_commodity[, CD$weight_column[CD$file == f] ]) ) / 1000
-						
+		# merge the two files by short hs code
+		
+		# add file/comtrade ratio column
+		
+		# reorganize the weights to be next to each other maybe
+		
+		# write file, will be one per file
+		
+		# one more thing that will be useful is having one file per commodity
+		# so create vectors of codes
+		# filter resulting table for codes 
+		# write them
+		
 			if (CD$year[CD$file == f] == 2005){ comtrade <- comtrade05 }
 			if (CD$year[CD$file == f] == 2006){ comtrade <- comtrade06 }
 			if (CD$year[CD$file == f] == 2007){ comtrade <- comtrade07 }
@@ -132,7 +62,7 @@ for (cc in countries){
 			if (CD$year[CD$file == f] == 2017){ comtrade <- comtrade16 }
 			if (CD$year[CD$file == f] == 2018){ comtrade <- comtrade16 }
 				
-			comtrade <- comtrade[(comtrade$country == CD$comtrade_country[CD$file == f]) & (comtrade$commodity %in% hs6_commodity),  ]
+			comtrade <- comtrade[comtrade$country == CD$comtrade_country[CD$file == f],  ]
 				
 			weights_table$comtrade[i] <- sum(as.numeric( comtrade$comtrade_weight )) / 1000
 			weights_table$deviation[i] <- weights_table$new_column[i] / weights_table$comtrade[i]
@@ -142,10 +72,6 @@ for (cc in countries){
 		if (CD$year[CD$file == f] == 2017){ year <- 2016 }
 		if (CD$year[CD$file == f] == 2018){ year <- 2016 }
 		
-		if (cc == 'COSTARICA'){names(weights_table)[names(weights_table) == 'new_column'] <- paste0( f , ' tons')}
-		names(weights_table)[names(weights_table) == 'new_column'] <- paste0( strsplit(f, paste0('/', cc))[[1]][2] , ' tons')
-		names(weights_table)[names(weights_table) == 'comtrade'] <- paste0('comtrade_tons_', year)
-		names(weights_table)[names(weights_table) == 'deviation'] <- paste0('trase_per_comtrade')
 	}
 	
 	write.table(weights_table, paste0(current_folder, '/', 'CD_weights_', cc, '.csv'), quote = FALSE, row.names = FALSE, dec = '.', sep = ';')
